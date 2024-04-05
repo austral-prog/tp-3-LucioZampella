170d51150d469490514c920db47fb5a6fb2d7fb8
LucioZampella <162721696+LucioZampella@users.noreply.github.com>
2024-04-05 11:07:59 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,5 +1,5 @@
 def slice_simple():
    texto = "Awesome"
-    print(f"{texto}".lower()[0:3])
-    print(f"{texto}".lower()[2:5])
-    print(f"{texto}".lower()[0:4] + f"{texto}".lower()[4:8])
+   print(f"{texto}".lower()[0:3])
+   print(f"{texto}".lower()[2:5])
+   print(f"{texto}".lower()[0:4] + f"{texto}".lower()[4:8])
